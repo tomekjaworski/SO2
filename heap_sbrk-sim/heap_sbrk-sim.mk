f80c10ce8b2bf04f7e7cdbca34ff09ba5fc184f2
Tomasz Jaworski <tjaworski@iis.p.lodz.pl>
2019-11-17 15:28:10 +0100

    Projekt 2: Symulacja funkcji sbrk()

---
+++ heap_sbrk-sim/heap_sbrk-sim.mk
@@ -0,0 +1,114 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Debug
+ProjectName            :=heap_sbrk-sim
+ConfigurationName      :=Debug
+WorkspacePath          :=/root/Desktop/SO2_Wyklady
+ProjectPath            :=/root/Desktop/SO2_Wyklady/heap_sbrk-sim
+IntermediateDirectory  :=$(ConfigurationName)
+OutDir                 := $(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=root
+Date                   :=11/17/19
+CodeLitePath           :=/root/.codelite
+LinkerName             :=/usr/bin/g++
+SharedObjectLinkerName :=/usr/bin/g++ -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputFile             :=$(IntermediateDirectory)/$(ProjectName)
+Preprocessors          :=
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :="heap_sbrk-sim.txt"
+PCHCompileFlags        :=
+MakeDirCommand         :=mkdir -p
+LinkOptions            :=  
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                := $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
+##
+AR       := /usr/bin/ar rcu
+CXX      := /usr/bin/g++
+CC       := /usr/bin/gcc
+CXXFLAGS :=  -g -O0 -Wall $(Preprocessors)
+CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := /usr/bin/as
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=/usr/share/codelite
+Objects0=$(IntermediateDirectory)/main.c$(ObjectSuffix) $(IntermediateDirectory)/memmanager.c$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) $(@D)
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@test -d $(ConfigurationName) || $(MakeDirCommand) $(ConfigurationName)
+
+
+$(IntermediateDirectory)/.d:
+	@test -d $(ConfigurationName) || $(MakeDirCommand) $(ConfigurationName)
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/main.c$(ObjectSuffix): main.c
+	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.c$(ObjectSuffix) -MF$(IntermediateDirectory)/main.c$(DependSuffix) -MM main.c
+	$(CC) $(SourceSwitch) "/root/Desktop/SO2_Wyklady/heap_sbrk-sim/main.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.c$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/main.c$(PreprocessSuffix): main.c
+	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.c$(PreprocessSuffix) main.c
+
+$(IntermediateDirectory)/memmanager.c$(ObjectSuffix): memmanager.c
+	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/memmanager.c$(ObjectSuffix) -MF$(IntermediateDirectory)/memmanager.c$(DependSuffix) -MM memmanager.c
+	$(CC) $(SourceSwitch) "/root/Desktop/SO2_Wyklady/heap_sbrk-sim/memmanager.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/memmanager.c$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/memmanager.c$(PreprocessSuffix): memmanager.c
+	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/memmanager.c$(PreprocessSuffix) memmanager.c
+
+
+-include $(IntermediateDirectory)/*$(DependSuffix)
+##
+## Clean
+##
+clean:
+	$(RM) -r $(ConfigurationName)/
+
+
---
+++ heap_sbrk-sim/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "custom_unistd.h"
+
+
+int main(int argc, char **argv)
+{
+
+    // Tutaj można pisać kod
+    
+	printf("hello world\n");
+	return 0;
+}
