5128e5edd03275f8a822076e7963434005324e3a
Tomasz Jaworski <tjaworski@iis.p.lodz.pl>
2021-01-12 11:21:29 +0100

    pdec: korekta

--- pdec/main.c
+++ pdec/main.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int counter = 1000;
+volatile int counter = 1000;
 
 void* dec(void*arg) {
 	for (int i = 0; i < 10; i++) {
 		int value = counter;
-		usleep(1000);
+		usleep(100); // można usunąc
 		value--;
-		usleep(1000);
+		usleep(100);
 		counter = value;
 	}
 	return NULL;
@@ -18,14 +18,19 @@ void* dec(void*arg) {
 
 
 int main() {
-	pthread_t th[10];
-	for (int i = 0; i < 10; i++)
-		pthread_create(&th[i], NULL, dec, NULL);
+    
+    for (int j = 0; j < 1000; j++ ){
+        counter = 1000;
+        pthread_t th[10];
+        for (int i = 0; i < 10; i++)
+            pthread_create(&th[i], NULL, dec, NULL);
 
-	for (int i = 0; i < 10; i++)
-		pthread_join(th[i], NULL);
+        for (int i = 0; i < 10; i++)
+            pthread_join(th[i], NULL);
 
-	printf("counter=%d\n", counter);
+        printf("counter=%d\n", counter);
+    
+    }
 	return 0;
 }
 
